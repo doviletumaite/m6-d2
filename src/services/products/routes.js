8d4ba9edb6de2b378cabe7d1f6e4c158dde7a0b2
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-09-28 16:13:31 +0200

    getid, put, delete

--- src/services/products/routes.js
+++ src/services/products/routes.js
@@ -44,4 +44,56 @@ route.post("/", async (req, res, next) => {
     res.status(500).send(error)   
    }
 })
+
+route.get("/:id", async (req, res, next) => {
+    try {
+       const query =  `SELECT * FROM products WHERE id=${req.params.id};`
+       const result = await pool.query(query) 
+       if (result.rows.length > 0) {
+           const product = result.rows[0]
+        //    const reviewsQuery = `SELECT * FROM reviews WHERE id=${req.params.id};`
+        //    const reviewResult = await pool.query(reviewsQuery)
+        //    const reviews = reviewResult.rows
+           res.send({product})
+       }
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error) 
+    }
+})
+
+route.delete("/:id", async (req, res, next) => {
+    try {
+        const query = `DELETE FROM products WHERE id=${req.params.id};` 
+        await pool.query(query)
+        res.status(204).send() 
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)   
+    }
+})
+
+route.put("/:id", async (req, res, next) => {
+    try {
+        const {name, description, brand, image_url, price, category} = req.body 
+        const query =  `
+       UPDATE products
+       SET
+        name=${"'"+name+"'"},
+        description=${"'"+description+"'"},
+        brand=${"'"+brand+"'"},
+        image_url=${"'"+image_url+"'"},
+        price=${"'"+price+"'"},
+        category=${"'"+category+"'"},
+        updated_at= NOW()
+        WHERE id=${req.params.id}
+       RETURNING *;
+       `   
+       const result = await pool.query(query)
+       res.send(result.rows[0])
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)    
+    }
+})
 export default route
